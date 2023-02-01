306632f9a162f07d5400a146038744d4e996226d
ennioitaliano <ennio.italiano@gmail.com>
2023-02-01 17:08:04 +0000

    Validazione search results

--- search_results.php
+++ search_results.php
@@ -15,7 +15,7 @@ $header = str_replace('<breadcrumb />', $breadcrumb, $header);
 $search_results = str_replace('<php-header />', $header, $search_results);
 
 $search = new Search_Results_Control();
-$risultati = $search->getResults($_GET['search'], $_GET['categoria'], isset($_GET['ordine'])?$_GET['ordine']:"standard");
+$risultati = $search->getResults($_GET['search'], $_GET['categoria'], isset($_GET['ordine']) ? $_GET['ordine'] : "standard");
 $listings = '<h2> Ricerca ' . $_GET['categoria'] . ' "' . $_GET['search'] . '"</h2>';
 $listings .= listingsList($risultati, $_GET['categoria']);
 
@@ -24,19 +24,19 @@ $search_results = str_replace('<php-listings />', $listings, $search_results);
 $search_results = str_replace('<php-footer />', $footer, $search_results);
 $search_results = str_replace('php-search-value', $_GET['search'], $search_results);
 
-$search_results = str_replace('php-catlibri', $_GET['categoria']=="libri"?"selected":"", $search_results);
-$search_results = str_replace('php-catappunti', $_GET['categoria']=="appunti"?"selected":"", $search_results);
-$search_results = str_replace('php-catripetizioni', $_GET['categoria']=="ripetizioni"?"selected":"", $search_results);
+$search_results = str_replace('php-catlibri', $_GET['categoria'] == "libri" ? "selected" : "", $search_results);
+$search_results = str_replace('php-catappunti', $_GET['categoria'] == "appunti" ? "selected" : "", $search_results);
+$search_results = str_replace('php-catripetizioni', $_GET['categoria'] == "ripetizioni" ? "selected" : "", $search_results);
 
 $search_results = str_replace('php-title-search', $_GET['search'], $search_results);
 $search_results = str_replace('php-type-search', $_GET['categoria'], $search_results);
 
 if (!isset($_GET['ordine'])) {
-    $search_results = str_replace('php-orderstandard', "selected", $search_results);
+    $search_results = str_replace('<option value="standard">', '<option value="standard" selected>', $search_results);
 } else {
-    $search_results = str_replace('php-orderstandard', $_GET['ordine']=="standard"?"selected":"", $search_results);
-    $search_results = str_replace('php-orderpreasc', $_GET['ordine']=="prezzoasc"?"selected":"", $search_results);
-    $search_results = str_replace('php-orderpredisc', $_GET['ordine']=="prezzodisc"?"selected":"", $search_results);
+    $search_results = str_replace('<option value="standard">', $_GET['ordine'] == "standard" ? '<option value="standard selected">' : '<option value="standard">', $search_results);
+    $search_results = str_replace('<option value="prezzoasc">', $_GET['ordine'] == "prezzoasc" ? '<option value="prezzoasc" selected>' : '<option value="prezzoasc">', $search_results);
+    $search_results = str_replace('<option value="prezzodisc">', $_GET['ordine'] == "prezzodisc" ? '<option value="prezzodisc" selected>' : '<option value="prezzodisc">', $search_results);
 }
 // Mostro la pagina
 echo $search_results;
